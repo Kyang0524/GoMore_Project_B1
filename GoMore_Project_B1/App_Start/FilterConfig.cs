4a7230ff2e526c7f81d0b6d2b6174aba8fadf671
KY <xiaoyang7481@gmail.com>
2023-10-14 20:44:59 +0800

    加入專案檔案。

---
+++ GoMore_Project_B1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GoMore_Project_B1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GoMore_Project_B1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoMore_Project_B1.MvcApplication" Language="C#" %>
